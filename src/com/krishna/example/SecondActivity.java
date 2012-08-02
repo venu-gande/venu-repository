76d7940ff3d6a088bbaf78f883b38439d15b8987
venu gandhe <venu.gandhe@gmail.com>
2012-08-02 12:15:02 +0200

    second activity added

---
+++ src/com/krishna/example/SecondActivity.java
@@ -0,0 +1,24 @@
+package com.krishna.example;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class SecondActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_second, menu);
+        return true;
+    }
+
+    
+}
